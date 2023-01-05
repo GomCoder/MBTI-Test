6ebc77c16840388427abbb84e43fe804968400ec
GomCoder <k2001911@hanmail.net>
2023-01-06 01:03:09 +0900

    결과 페이지 작업(CSS, JS(결과 출력 기능))

---
+++ js/results.js
@@ -0,0 +1,33 @@
+import {results, mbtis} from './data.js'; 
+
+const mbti = new URLSearchParams(location.search).get('mbti');
+const result = results[mbtis[mbti]];
+
+const titleEl = document.querySelector('.page-title');
+const characterEl = document.querySelector('.character');
+const boxEls = document.querySelectorAll('.box');
+const jobEls = document.querySelectorAll('.job');
+const lectureEl = document.querySelector('.lecture');
+const lectureImgEl = document.querySelector('.lecture img');
+
+//타이틀 출력
+titleEl.innerHTML = result.title;
+
+//결과 출력
+characterEl.src = result.character;
+boxEls.forEach(function(boxEl, index) {
+    boxEl.innerHTML = result.results[index];
+});
+
+jobEls.forEach(function(jobEl, index) {
+    jobEl.innerHTML = result.jobs[index];
+});
+
+//추천 강의 이미지
+lectureEl.href = result.lectureUrl;
+lectureImgEl.src = result.lectureImg;
+
+
+
+
+
